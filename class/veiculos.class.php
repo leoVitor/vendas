718f06d25ef571415dac149f41ddfba8e6e26852
Vitor Diniz Dantas <vitor@localhost.localdomain>
2018-05-16 20:23:33 -0300

    Classe Estado

---
+++ class/veiculos.class.php
@@ -0,0 +1,50 @@
+<?php
+    class Veiculo{
+    	private $idveiculo;
+    	private $modelo;
+    	private $ano;
+    	private $placa;
+    	private $chassi;
+		private $preco;
+		
+
+    	public function setIdveiculo($idveiculo){
+    		$this->idveiculo = $idveiculo;
+    	}
+    	public function setModelo($modelo){
+    		$this->modelo = $modelo;
+    	}
+    	public function setAno($ano){
+    		$this->ano = $ano;
+    	}
+    	public function setPlaca($placa){
+    		$this->placa = $placa;
+    	}
+    	public function setChassi($chassi){
+    		$this->chassi = $chassi;
+    	}
+    	public function setPreco($preco){
+    		$this->preco = $preco;
+    	}
+    	
+    	public function getIdveiculo(){
+    		return $this->idveiculo;
+    	}
+    	public function getModelo(){
+    		return $this->modelo;
+    	}
+    	public function getAno(){
+    		return $this->ano;
+    	}
+    	public function getPlaca(){
+    		return $this->placa;
+    	}
+    	public function getChassi(){
+    		return $this->chassi;
+    	}
+    	public function getPreco(){
+    		return $this->preco;
+    	}
+
+    }
+?>
