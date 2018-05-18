5044473d62161e7f2ca336eb5395df4db0461b5b
Vitor Diniz Dantas <vitor@localhost.localdomain>
2018-05-18 20:39:32 -0300

    Class

---
+++ class/endereco.class.php
@@ -0,0 +1,15 @@
+<?php
+
+    class Endereco{
+
+        private $id_endereco;
+        private $logradouro;
+        private $cep;
+        private $numero;
+        private $complemento;
+        private $cidade_id_cidade;
+
+    }
+
+
+?>
